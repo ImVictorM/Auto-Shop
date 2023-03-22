5001b74a7f48e11c7c6656aff0f21a43ceeb5845
Victor Mendes <victorutdsim7@gmail.com>
2023-03-22 17:44:33 -0300

    feat: metodo para mapear um carro criado

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,6 +1,7 @@
 import ICar from '../Interfaces/ICar';
 import Service from './Service';
 import CarODM from '../Models/CarODM';
+import Car from '../Domains/Car';
 
 class CarService extends Service<CarODM> {
   constructor() {
@@ -8,10 +9,16 @@ class CarService extends Service<CarODM> {
     super(odm);
   }
 
-  public async createOne(carFromReq: ICar): Promise<ICar> {
-    const createdCar = this.odm.create(carFromReq);
-    return createdCar;
+  public async createOne(carFromReq: ICar): Promise<Car> {
+    const createdCar = await this.odm.create(carFromReq);
+    const car = this.mapCar(createdCar);
+    return car;
   } 
+
+  private mapCar(carFromReq: ICar) {
+    const mappedCar = new Car(carFromReq);
+    return mappedCar;
+  }
 }
 
 export default CarService;
