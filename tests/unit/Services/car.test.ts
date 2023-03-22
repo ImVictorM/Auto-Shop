24b42655e985cbee752fa552b2e6b4835496c858
Victor Mendes <victorutdsim7@gmail.com>
2023-03-22 20:20:32 -0300

    test: criar testes de unidade para CarService

---
+++ tests/unit/Services/car.test.ts
@@ -0,0 +1,60 @@
+import chai from 'chai';
+import Sinon from 'sinon';
+import CarODM from '../../../src/Models/CarODM';
+import CarService from '../../../src/Services/CarService';
+import { VALID_CAR_FROM_REQ, ALL_CARS, VALID_CAR_FROM_DB, VALID_ID } from './mocks/car.mock';
+
+const { expect } = chai;
+
+describe('Testing CarService', function () {
+  const service = new CarService();
+
+  afterEach(function () {
+    Sinon.restore();
+  });
+
+  it('Can create a car succesffully', async function () {
+    Sinon.stub(CarODM.prototype, 'createOne').resolves({ ...VALID_CAR_FROM_REQ, id: '123456' });
+
+    const response = await service.createOne(VALID_CAR_FROM_REQ);
+
+    expect(response).to.be.deep.equal({ ...VALID_CAR_FROM_REQ, id: '123456' });
+  });
+
+  it('Can get all cars', async function () {
+    Sinon.stub(CarODM.prototype, 'getAll').resolves(ALL_CARS);
+
+    const response = await service.getAll();
+
+    expect(response).to.be.deep.equal(ALL_CARS);
+  });
+
+  it('Can get a car by its id', async function () {
+    Sinon.stub(CarODM.prototype, 'getById').resolves(VALID_CAR_FROM_DB);
+
+    const response = await service.getById(VALID_CAR_FROM_DB.id);
+
+    expect(response).to.be.deep.equal(VALID_CAR_FROM_DB);
+  });
+
+  it('throws an exception when the car id isn\'t valid', async function () {
+    Sinon.stub(CarODM.prototype, 'getById').resolves();
+
+    try {
+      await service.getById('123');
+    } catch (error) {
+      expect((error as Error).message).to.be.equal('Invalid mongo id');
+      expect((error as Error).stack).to.be.equal('422');
+    }
+  });
+  it('throws an exception when the car doesn\'t exist', async function () {
+    Sinon.stub(CarODM.prototype, 'getById').resolves(null);
+
+    try {
+      await service.getById(VALID_ID);
+    } catch (error) {
+      expect((error as Error).message).to.be.equal('Car not found');
+      expect((error as Error).stack).to.be.equal('404');
+    }
+  });
+});
---
+++ tests/unit/Services/mocks/car.mock.ts
@@ -0,0 +1,40 @@
+export const VALID_CAR_FROM_REQ = {
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const ALL_CARS = [
+  {
+    id: '634852326b35b59438fbea2f',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '634852326b35b59438fbea31',
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    status: false,
+    buyValue: 39,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+];
+
+export const VALID_ID = '634852326b35b59438fbea31';
+
+export const VALID_CAR_FROM_DB = {
+  ...ALL_CARS[1],
+};
+
+export default VALID_CAR_FROM_REQ;
