9375497eefbd9637cfddad1746b6146fa2f6e1d6
Victor Mendes <victorutdsim7@gmail.com>
2023-03-23 15:17:57 -0300

    feat: cria odm para motos

---
+++ src/Models/MotorcycleODM.ts
@@ -0,0 +1,22 @@
+import { Schema } from 'mongoose';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import AbstractODM from './AbstractODM';
+
+class MotorcycleODM extends AbstractODM <IMotorcycle> {
+  constructor() {
+    const motorcycleSchema = new Schema<IMotorcycle>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean },
+      buyValue: { type: Number, required: true },
+      category: { type: String, required: true },
+      engineCapacity: { type: Number, required: true },
+    });
+
+    const modelName = 'motorcycles';
+    super(motorcycleSchema, modelName);
+  }
+}
+
+export default MotorcycleODM;
